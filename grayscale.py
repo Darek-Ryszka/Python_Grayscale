0a9389a63797ddf3be8c76941ccd7172f975dc99
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-09 09:41:15 +0200

    Add converting image

--- grayscale.py
+++ grayscale.py
@@ -10,3 +10,7 @@ args = parser.parse_args()
 for path in glob(args.input + '/*'):
     directory, filename = path.split('\\')
     print(path, directory, filename)
+
+    with Image.open(path) as new_image:
+        grayscale_image = new_image.convert('L')
+        grayscale_image.save(args.output + '/' + filename)
