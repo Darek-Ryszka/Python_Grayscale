9c6d56d3ba97503f90be64218dfe0d0b0bd2282b
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-06-09 09:35:48 +0200

    Add listing image

--- grayscale.py
+++ grayscale.py
@@ -1,6 +1,11 @@
+from glob import glob
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Changing pictures on grayscale.')
 parser.add_argument('--input', help='Color image.', required=True)
 parser.add_argument('--output', help='Grayscale image.', required=True)
 args = parser.parse_args()
+
+for path in glob(args.input + '/*'):
+    directory, filename = path.split('\\')
+    print(path, directory, filename)
